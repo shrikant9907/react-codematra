5090cf2df15e5a7dabfaaefc85685a0d0cc2db00
Shrikant Yadav <shrikant9907@gmail.com>
2021-10-10 23:07:28 +0530

    env files

--- how-to-codematra/src/App.js
+++ how-to-codematra/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          { process.env.REACT_APP_API_URL }
         </p>
         <a
           className="App-link"
