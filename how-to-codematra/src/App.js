1e338414cb8fb532f5ce8fb6d93394f679084773
Shrikant Yadav <shrikant9907@gmail.com>
2021-10-15 19:12:06 +0530

    modal trigger button

--- how-to-codematra/src/App.js
+++ how-to-codematra/src/App.js
@@ -1,9 +1,23 @@
-import ContactForm from "./components/contact-form/ContactForm";
+import React, { useState } from 'react';
+import Modal from "./components/modal/Modal";
 
 function App() { 
+
+  // Modal Flag: Default false means hide
+  const [showModal, setShowModal]     = useState(false);
+
+  // Method to show / hide the modal
+  const handleToggleModal = () => {
+    setShowModal(!showModal);
+  }
+ 
   return (
     <>
-     <ContactForm></ContactForm>
+     {/* Modal Trigger  */}
+     <button className="btnui margin-top-50" onClick={handleToggleModal}>Toggle Button</button>
+
+     {/* Modal Component */}
+     <Modal show={showModal} close={handleToggleModal} />
     </>
   );
 }
