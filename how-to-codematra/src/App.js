ea76072cf9f98c9829be62ad76699c9c1e2bb2ae
Shrikant Yadav <shrikant9907@gmail.com>
2021-10-16 12:52:48 +0530

    styles added for navigation

--- how-to-codematra/src/App.js
+++ how-to-codematra/src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import AboutPage from "./components/pages/AboutPage";
 import ContactPage from "./components/pages/ContactPage";
+import './components/pages/PageStyle.scss';
 
 function App() { 
   return (
     <Router>
 
-      <nav>
+      <nav className="navui1">
         <ul>
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/about'>About Us</Link></li>
@@ -16,11 +17,13 @@ function App() {
         </ul>
       </nav>
 
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/contact" component={ContactPage}/>
-      </Switch>
+      <div className="pagewr">
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/contact" component={ContactPage}/>
+        </Switch>
+      </div>
 
     </Router>
   );
